69eb0ddeee878de2832a968a322895cdecde7b49
Saksham Checker <checkersaksham@yahoo.com>
2022-02-05 22:06:05 +0530

    train linknet

--- train.py
+++ train.py
@@ -22,6 +22,7 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 from utilities.data_gen import *
 from utilities.unet import get_unet
+from utilities.linknet import model as lnm
 
 
 def unet():
@@ -65,6 +66,37 @@ def unet():
     plt.show()
 
 
+def linknet_t():
+    model=lnm
+    model.compile(optimizer='adam', loss="binary_crossentropy", metrics=[tf.keras.metrics.MeanSquaredError()])
+    callbacks = [
+        EarlyStopping(patience=10, verbose=1),
+        ReduceLROnPlateau(factor=0.1, patience=5, min_lr=1e-5, verbose=1),
+        ModelCheckpoint('model/model-linknet.h5', verbose=1, save_best_only=True, save_weights_only=True)
+    ]
+    model.summary()
+    # STEP_SIZE_TRAIN = timage_generator.n/BATCH_SIZE
+    # STEP_SIZE_VALID = vimage_generator.n/BATCH_SIZE
+    # results = model.fit(train_gen,
+    #                     steps_per_epoch=STEP_SIZE_TRAIN,
+    #                     batch_size=BATCH_SIZE,
+    #                     epochs=EPOCHS,
+    #                     callbacks=callbacks,
+    #                     validation_data=valid_gen,
+    #                     validation_steps=STEP_SIZE_VALID)
+    # from matplotlib import pyplot as plt
+    # import seaborn as sns
+    # plt.figure(figsize=(8, 8))
+    # plt.title("Learning curve")
+    # plt.plot(results.history["loss"], label="loss", color=sns.xkcd_rgb['greenish teal'])
+    # plt.plot(results.history["val_loss"], label="val_loss", color=sns.xkcd_rgb['amber'])
+    # plt.plot( np.argmin(results.history["val_loss"]), np.min(results.history["val_loss"]), marker="x", color="r", label="best model")
+    # plt.xlabel("Epochs")
+    # plt.ylabel("log_loss")
+    # plt.legend()
+    # # plt.grid(False)
+    # plt.show()
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
@@ -73,3 +105,5 @@ if __name__ == "__main__":
     
     if args.query == 'unet':
         unet()
+    if args.query=='linknet':
+        linknet_t()
